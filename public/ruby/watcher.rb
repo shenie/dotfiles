0da727c43da10d010fc5d13f36114aad673ec653
Andy Shen <andy@shenie.net>
2011-06-29 19:34:44 +1000

    Don't need a new watchr file just to take a tag
    Run it like this
    watchr watchr.rb -- "-t wip"

--- public/ruby/watcher.rb
+++ public/ruby/watcher.rb
@@ -1,11 +1,10 @@
-
 watch( 'app/.*\.rb' )  { |md| 
   if md[0] =~ /(models|controllers)/
     puts '=' * 100
     spec_file = "spec/#{md[0].split('.').first}_spec.rb".gsub(/\/app\//, '/')
     if File.exists?(spec_file)
       puts "Running #{spec_file}"
-      system("bundle exec rspec --format doc #{spec_file}")
+      system("bundle exec rspec #{ARGV[1..-1].join(' ')} --format doc #{spec_file}")
     end
   end
 }
@@ -13,7 +12,7 @@ watch( 'app/.*\.rb' )  { |md|
 watch( '.*_spec.rb' )  { |md| 
   puts '=' * 100
   puts "Running #{md[0]}"
-  system("bundle exec rspec --format doc #{md[0]}")
+  system("bundle exec rspec #{ARGV[1..-1].join(' ')} --format doc #{md[0]}")
 }
 
 watch( '.*.feature' )  { |md| 
