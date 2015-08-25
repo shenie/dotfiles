3563d55c364e3b455de8ac955734df229cedb237
Andy Shen <andy@shenie.net>
2015-08-25 13:17:38 +1000

    support grails 3, setup for sm-strap via bower

--- public/bin/set_grails.sh
+++ public/bin/set_grails.sh
@@ -11,3 +11,14 @@ if [ -e application.properties ] ; then
   fi
 fi
 
+if [ -e gradle.properties ] ; then
+  current=$(realpath $GRAILS_HOME |cut -d'/' -f6)
+
+  required=$(grep grailsVersion gradle.properties | cut -d= -f 2)
+
+  if [ "${required}" != "" -a "${current}" != "${required}" ] ; then
+    echo "Switching to grails ${required}"
+    gvm use grails ${required}
+  fi
+fi
+
