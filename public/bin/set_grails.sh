21c10f4a3b9b47ad25c48154aacd3ee2076c56f1
Andy Shen <andy@shenie.net>
2016-02-18 15:30:29 +1100

    switch to sdkman

--- public/bin/set_grails.sh
+++ public/bin/set_grails.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 if [ -e application.properties ] ; then
-  current=$(realpath $GRAILS_HOME |cut -d'/' -f6)
+  current=$(realpath $GRAILS_HOME |cut -d'/' -f7)
 
   required=$(grep grails.version application.properties | cut -d= -f 2)
 
   if [ "${required}" != "" -a "${current}" != "${required}" ] ; then
     echo "Switching to grails ${required}"
-    gvm use grails ${required}
+    sdk use grails ${required}
   fi
 fi
 
 if [ -e gradle.properties ] ; then
-  current=$(realpath $GRAILS_HOME |cut -d'/' -f6)
+  current=$(realpath $GRAILS_HOME |cut -d'/' -f7)
 
   required=$(grep grailsVersion gradle.properties | cut -d= -f 2)
 
   if [ "${required}" != "" -a "${current}" != "${required}" ] ; then
     echo "Switching to grails ${required}"
-    gvm use grails ${required}
+    sdk use grails ${required}
   fi
 fi
 
