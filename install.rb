346110b20c7c53f3acd2d871fbe948d51c4384c9
Andy Shen <andy@shenie.net>
2017-03-04 10:57:25 +1100

    Handle machine without and custom mappings

--- install.rb
+++ install.rb
@@ -18,13 +18,11 @@ HOST = `hostname`.split('.').first.strip
 raise "#{PRIVATE_DIR} does not exists" unless File.exist? PRIVATE_DIR
 
 CONFIG = YAML::load(ERB.new(File.readlines("#{PRIVATE_DIR}/config.yml").join).result)
-SETTINGS = CONFIG[HOST]
-
-abort "NO SETTINGS FOUND in config for host #{HOST}" if SETTINGS.nil?
+SETTINGS = CONFIG[HOST] || {}
 
 def install_link(src, file)
-  link = file =~ /^\// ? file : "#{HOME}/#{file}" 
-  
+  link = file =~ /^\// ? file : "#{HOME}/#{file}"
+
   if File.exist?(link) && !File.symlink?(link) && !File.identical?(link, src)
     printf "        #{link} is not a symlink and different to #{src}\n"
   else
@@ -39,6 +37,6 @@ mkdir BIN_DIR unless File.exist? BIN_DIR
 cp_r(Dir["#{PUBLIC_DIR}/bin/*"], BIN_DIR)
 
 # Install symlinked files
-[SETTINGS, CONFIG].collect {|h| h['mappings'] }.inject({}) { |r, h| r.merge!(h) if h; r }.each do |file, src| 
+[SETTINGS, CONFIG].collect {|h| h['mappings'] }.inject({}) { |r, h| r.merge!(h) if h; r }.each do |file, src|
   install_link(src, file)
 end
