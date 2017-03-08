749854669c536ff3d3bfafb0d6dfb4b55753b10f
Andy Shen <andy@shenie.net>
2017-03-08 13:42:43 +1100

    GRAILS_HOME might be dir instead of symlink
    Just use sdk to figure out current version.
    No need to echo as sdk use will write to stdout already.

--- public/bash/bin/set_grails.sh
+++ public/bash/bin/set_grails.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
+current=$(sdk current grails | cut -d' ' -f4)
+
 if [ -e application.properties ] ; then
-  current=$(readlink $GRAILS_HOME |cut -d'/' -f7)
 
   required=$(grep grails.version application.properties | cut -d= -f 2)
 
   if [ "${required}" != "" -a "${current}" != "${required}" ] ; then
-    echo "Switching to grails ${required}"
     sdk use grails ${required}
   fi
 fi
 
 if [ -e gradle.properties ] ; then
-  current=$(readlink $GRAILS_HOME |cut -d'/' -f7)
 
   required=$(grep grailsVersion gradle.properties | cut -d= -f 2)
 
   if [ "${required}" != "" -a "${current}" != "${required}" ] ; then
-    echo "Switching to grails ${required}"
     sdk use grails ${required}
   fi
 fi
