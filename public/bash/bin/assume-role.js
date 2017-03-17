4270449669e2cffb322c23ddcb70b7d2bf9b7c4c
Andy Shen <andy@shenie.net>
2017-03-17 16:40:24 +1100

    shortcut to assume role over cli

---
+++ public/bash/bin/assume-role.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').exec
+
+let account = process.argv[2]
+
+exec(`aws sts assume-role --role-arn 'arn:aws:iam::${account}:role/Admin' --role-session-name ashen-session-${Math.random()}`, (err, stdout, stderr) => {
+
+  if (err) {
+    throw new Error(`failed to run assume-role: ${stderr}` , err)
+  }
+
+  let payload = JSON.parse(stdout)
+
+  console.log(`export AWS_SECRET_ACCESS_KEY=${payload.Credentials.SecretAccessKey}`)
+  console.log(`export AWS_ACCESS_KEY_ID=${payload.Credentials.AccessKeyId}`)
+  console.log(`export AWS_SESSION_TOKEN=${payload.Credentials.SessionToken}`)
+
+  console.log(`# eval $(node assume-role.js ${account})`)
+})
